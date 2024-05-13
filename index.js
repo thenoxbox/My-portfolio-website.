b00df383875c770584eb6a9e4338db1cd8c21ac2
Enoxify <lordyam307@gmail.com>
2024-05-13 15:14:21 +0100

    minor changes.

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const menu_button = $(".hamburger");
 const mobile_menu = $(".mobile-nav")
+const bretheren = $(".bros")
 menu_button.click(function () {
+    menu_button.toggleClass("is-active"); 
+    mobile_menu.toggleClass("is-active");
+});
+
+bretheren.click(function () {
     menu_button.toggleClass("is-active"); 
     mobile_menu.toggleClass("is-active");
 });
